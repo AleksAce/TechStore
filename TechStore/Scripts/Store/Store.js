b78004821ed7d9321d759d9c922e03ca86e0fe7a
ace.aleks93 <ace.aleks93@gmail.com>
2018-03-16 22:30:07 +0100

    added scroll to top button

---
+++ TechStore/Scripts/Store/Store.js
@@ -0,0 +1,20 @@
+﻿    //Note: Use this with main-content and preloader classes when we wait
+    var showLoader = function (interval) {
+        $('.main-content').css('display', 'none');
+        $('#preloader').css('display', 'initial');
+       var interval =setInterval(function () {
+        $('.main-content').fadeIn(500);
+    $('#preloader').css('display', 'none');
+        }, interval)
+    };
+    $(window).on("load", function () {
+        $('.main-content').fadeIn(500);
+        $('#preloader').css('display', 'none');
+        //showLoader(5000);
+    });
+
+    $(document).ready(function () {
+        $('#scroll-to-top').click(function () {
+            $("html").animate({scrollTop:0}, "slow");
+        });
+    });
